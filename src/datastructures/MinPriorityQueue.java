4e7426c833ff064dad99fbabbd44756fe55180f3
MrKnyaz <>
2012-11-01 17:53:14 +0600

    .

---
+++ src/datastructures/MinPriorityQueue.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructures;
+
+/**
+ *
+ * @author Knyazev Alexander <aknyazev@kkb.kz>
+ */
+public class MinPriorityQueue<T> {
+    
+}
