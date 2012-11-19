f4c010eb78113ec7abf332b21c6bf124f475257a
MrKnyaz <>
2012-11-19 18:00:19 +0600

    .

--- src/datastructures/BinarySearchTree.java
+++ src/datastructures/BinarySearchTree.java
@@ -8,20 +8,41 @@ package datastructures;
  *
  * @author MrKnyaz
  */
-public class BinarySearchTree<T extends Comparable> {
-    
-    private int size;
+public class BinarySearchTree<K extends Comparable, E> {
+
     private Node root;
-    
+
     private class Node {
 
-        T item;
+        K key;
+        E value;
         Node right;
         Node left;
-        int height;
+        int size;
+
+        public Node(K key, E value, int size) {
+            this.key = key;
+            this.value = value;
+            this.size = size;
+        }
     }
-    
-    public void insert(T item) {
-        
+
+    public void put(K key, E value) {
+        root = insert(key, value, root);
+    }
+
+    private Node insert(K key, E value, Node node) {
+        if (node == null) {
+            return new Node(key, value, 1);
+        }
+        int compareResult = node.key.compareTo(key);
+        if (compareResult == 0) {
+            node.value = value;
+        } else if(compareResult > 0) {//new key is less than in current node
+            node.left = insert(key, value, node.left);
+        } else if (compareResult < 0) {
+            node.right = insert(key, value, node.right);
+        }
+        return node;
     }
 }
